210c3d2c72c17f75ae0db2851344a50da0965f53
hanicamunguambe <hanicamunguambe@gmail.com>
2021-11-28 21:20:30 +1200

    second commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+
+//// BUILD PROJECT
+
+1. mvn clean
+
+2. mvn install
+
+3. mvn spring-boot:run
+
+4. localhost:8081
+
+//// RUN RabbitMQ
+
+localhost:15672
+
+1. login
+
+2. open exchanges
+
+3. add quee in the name bellow rabbitMQConsumer
+
+
+//// POSTMAN
+
+1. open postman
+
+2. add the url
--- calculator_publihser/src/main/java/com/calculator_publisher/controller/CalculatorControler.java
+++ calculator_publihser/src/main/java/com/calculator_publisher/controller/CalculatorControler.java
@@ -10,13 +10,25 @@ import java.math.BigDecimal;
 public class CalculatorControler {
 
 
-
     @Autowired
     RabbitMQConsumer rabbitMQConsumer;
 
-
     public static BigDecimal addiction(BigDecimal a, BigDecimal b) {
+
         return a.add(b);
     }
+
+    public static BigDecimal subtraction (BigDecimal a, BigDecimal b){
+        return  a.subtract(b);
+    }
+
+    public static BigDecimal multiplication (BigDecimal a, BigDecimal b){
+        return  a.multiply(b);
+    }
+
+    public static BigDecimal division (BigDecimal a, BigDecimal b){
+        return  a.divide(b);
+    }
+
 
 }
